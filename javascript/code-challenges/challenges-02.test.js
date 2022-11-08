f1824b850af9160a5786d5926333ed262b172de7
ianfshirley <ianfshirley88@gmail.com>
2022-11-08 10:42:54 -0800

    challenge 2

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -25,6 +25,8 @@ Write a function named addOne that, given an array of numbers, uses map to retur
 
 const addOne = (arr) => {
   // Solution code here...
+  let plusOne = arr.map(n => n + 1);
+  return plusOne;
 };
 
 /* ------------------------------------------------------------------------------------------------
