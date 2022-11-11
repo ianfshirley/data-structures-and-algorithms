0f35ce1e8712161ba82ad6244a8df6eb0850178a
ianfshirley <ianfshirley88@gmail.com>
2022-11-10 21:50:54 -0800

    all tests passing

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -81,6 +81,7 @@ For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 
 const alphabetize = (arr) => {
   // Solution code here...
+  return arr.sort();
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -98,6 +99,9 @@ Here is an example of the input:
 
 const sortByPrice = (arr) => {
   // Solution code here...
+  return arr.sort((a,b) =>
+    a.price - b.price
+  );
 };
 
 /* ------------------------------------------------------------------------------------------------
