2560b24d53cfa3d334472cd20343559e1c62109c
ianfshirley <ianfshirley88@gmail.com>
2022-12-28 14:57:17 -0800

    all tests passing for CC7

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -10,6 +10,15 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+        # another way:
+        # new_node = Node(value)
+        # old_head = self.head
+        # self.head = new_node
+        # self.head.next = old_head
+
+        # in one line:
+        # self.head = Node(value, self.head)
+
     def includes(self, value):
         """
         indicates whether a value exists as a Node's value somewhere within the list
@@ -53,6 +62,21 @@ class LinkedList:
             current.next = new_node
         except Exception as e:
             raise TargetError(e)
+
+    def kth_from_end(self, value):
+        current = self.head
+        length = 0
+        while current is not None:
+            current = current.next
+            length += 1
+        if value >= length:
+            raise TargetError(Exception)
+        if value < 0:
+            raise TargetError(Exception)
+        current = self.head
+        for x in range(0, length - value - 1):
+            current = current.next
+        return current.value
 
     def __str__(self):
         values = []
