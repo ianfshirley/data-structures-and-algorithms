52f50a353528c781755b888b956ce48c275e3e56
ianfshirley <ianfshirley88@gmail.com>
2023-01-02 14:44:37 -0800

    started pseudo queue code

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,27 @@
 from data_structures.stack import Stack
 
 
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class PseudoQueue:
-    pass
+    """
+    create a pseudoqueue class to implement standard queue interface, internally using 2 stack instances to create and manage the queue
+    """
+
+    def __init__(self):
+        # self.front = None
+        # self.rear = None
+        stack_1 = Stack()
+        stack_2 = Stack()
+
+    def enqueue(self, value):
+        #peek to see if stack 1 has a self.top value
+        if stack_1.peek():
+            stack_1.pop()
+
+    def dequeue(self):
+        pass
