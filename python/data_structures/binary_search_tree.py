0004ae26aeacd26710c4e75cfbe59f0d2800525d
ianfshirley <ianfshirley88@gmail.com>
2023-01-09 20:57:18 -0800

    all tests passing for binary search tree

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -6,10 +6,48 @@ class BinarySearchTree(BinaryTree):
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def add(self, value):
+        """
+        following the rules of a binary search tree, this method adds a new node at the correct location within the binary tree
+        """
+        if self.root is None:
+            self.root = Node(value)
+            return
+        
+        # start pointer at root
+        current = self.root
 
-    def some_method(self):
-        # method body here
-        pass
+        while current:
+            # if number of the added node is less than the current node, check left
+            if value < current.value:
+                # add node only if there's space
+                if current.left is None:
+                    current.left = Node(value)
+                    return
+                # if there is no space, go left again
+                current = current.left
+            # if number of added node is greater than current node, check right
+            else:
+                # add node only if there's space
+                if current.right is None:
+                    current.right = Node(value)
+                    return
+                # if there's no space, go right again
+                current = current.right
+
+
+    def contains(self, value):
+        """
+        checks if a binary search tree contains a node with a specified value
+        """
+        if self.root is None:
+            return False
+        if self.root.value == value:
+            return True
+        if self.root.value > value:
+            self.root = self.root.left
+            return self.contains(value)
+        if self.root.value < value:
+            self.root = self.root.right
+            return self.contains(value)
+        
