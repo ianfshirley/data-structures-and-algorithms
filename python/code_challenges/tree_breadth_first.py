3a2606d863b35e9d0273bc7fabebe5d48d38804a
ianfshirley <ianfshirley88@gmail.com>
2023-01-10 16:23:18 -0800

    CC17, all tests passing, updated README and added whiteboard image

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,23 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def breadth_first():
-    pass
+
+def breadth_first(tree):
+    staging = []
+    result = []
+
+    if tree.root is None:
+        return []
+
+    staging.append(tree.root)
+
+    while len(staging) > 0:
+        current = staging.pop(0)
+        result.append(current.value)
+
+        if current.left is not None:
+            staging.append(current.left)
+        if current.right is not None:
+            staging.append(current.right)
+
+    return result
