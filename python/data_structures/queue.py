9ebad9dd6e6fe152cfbb528bb138eb9b7bdec1cf
ianfshirley <ianfshirley88@gmail.com>
2023-01-12 09:27:13 -0800

    fixed queue.py

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -16,6 +16,41 @@ class Queue:
 
     def __init__(self):
         self.front = None
+        self.back = None
+
+    def enqueue(self, value):
+        if self.back:
+            self.back.next = Node(value)
+            self.back = self.back.next
+            return
+        self.back = self.front = Node(value)
+
+    def dequeue(self):
+        try:
+            if self.front == self.back:
+                dequeued = self.front
+                self.front = self.back = None
+                return dequeued.value
+            dequeued = self.front
+            self.front = self.front.next
+            return dequeued.value
+        except Exception as e:
+            raise InvalidOperationError(e)
+
+    def peek(self):
+        try:
+            return self.front.value
+        except Exception as e:
+            raise InvalidOperationError(e)
+
+    def is_empty(self):
+        return self.front is None
+
+
+    # MY OLD WAY:
+
+    def __init__(self):
+        self.front = None
         self.rear = None
 
     def enqueue(self, value):
