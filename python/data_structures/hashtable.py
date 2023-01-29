15e2df000456232f94cc5084e2ce51628542de11
ianfshirley <ianfshirley88@gmail.com>
2023-01-29 15:28:06 -0800

    created test for hash function, passing test

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,12 +1,39 @@
+from data_structures.linked_list import Node, LinkedList
+
+
 class Hashtable:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
+
+    def __init__(self, size=1024):
+        self.size = size
+        self._buckets = [None] * size
+
+
+    def _hash(self, key):
+        hash = 0
+        for char in key:
+            hash += ord(char)
+        hash = (hash * 599) % self.size
+        return hash
+
+    def set(self):
+        # method body here
         pass
 
-    def some_method(self):
+
+    def get(self):
+        # method body here
+        pass
+
+
+    def has(self):
+        # method body here
+        pass
+
+
+    def keys(self):
         # method body here
         pass
--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -2,10 +2,38 @@ import pytest
 from data_structures.hashtable import Hashtable
 
 
+# Write tests to prove the following functionality:
+
+
+
+
+
+# Successfully returns null for a key that does not exist in the hashtable
+
+# Successfully returns a list of all unique keys that exist in the hashtable
+
+# Successfully handle a collision within the hashtable
+
+# Successfully retrieve a value from a bucket within the hashtable that has a collision
+
+# Successfully hash a key to an in-range value
+
+
+
 def test_exists():
     assert Hashtable
 
 
+# Successfully hash a key to an in-range value
+def test_hash():
+    hashtable = Hashtable()
+    actual = hashtable._hash("squid")
+    expected = 746
+    assert actual == expected
+
+
+# Setting a key/value to your hashtable results in the value being in the data structure
+# Retrieving based on a key returns the value stored
 @pytest.mark.skip("TODO")
 def test_get_apple():
     hashtable = Hashtable()
